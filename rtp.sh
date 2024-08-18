391a69514f64a7b5669bcb7bd0ff27dc13487b43
Kevin <48370722+KevinVan9@users.noreply.github.com>
2024-08-18 15:04:09 +1000

    Directly transcode and stream sink with ffmpeg

--- rtp.sh
+++ rtp.sh
@@ -1,43 +1,44 @@
-d_ip='192.168.0.45'
+d_ip='192.168.0.159'
 port='46000'
+node_name='auto_null'
 verbose=false
-log=false
+old=false
 
 print_usage() {
-  printf "Usage: -d <destination-ip> -p <destination-port>"
+  printf "Usage: \n-d <destination-ip> \n-p <destination-port> \n-v    verbose logging\n"
 }
 
-while getopts 'd:p:vl' flag; do
+while getopts 'd:p:n:v' flag; do
   case "${flag}" in
-    d) d_flag="${OPTARG}" ;;
-    f) p_flag="${OPTARG}" ;;
+    d) d_ip="${OPTARG}" ;;
+    p) port="${OPTARG}" ;;
+    n) node_name="${OPTSARG}" ;;
     v) verbose=true ;;
-    l) log=true ;;
+    o) old=true ;;
     *) print_usage
        exit 1 ;;
   esac
 done
 
-
-pactl unload-module module-rtp-send
-pactl unload-module module-null-sink
-pactl load-module module-null-sink sink_name=rtp channels=2 rate=44100
-pactl load-module module-rtp-send source=rtp.monitor channels=2 rate=44100 format=s16be destination_ip=127.0.0.1 port=46000
-pactl set-default-sink rtp
-
-if [ $log = true ]; then
-    if [ $verbose = true ]; then
-        ffmpeg -v debug -re -i rtp://127.0.0.1:46000 -acodec mp3 -ab 128k -ac 2 -f rtp rtp://$d_ip:$port 2>&1 | tee debug.log
-    else
-        ffmpeg -re -i rtp://127.0.0.1:46000 -acodec mp3 -ab 128k -ac 2 -f rtp rtp://$d_ip:$port 2>&1 | tee debug.log
-    fi
+if [ "$old" = true ]; then
+    src="rtp://127.0.0.1:46000"
+    fmt="s16be"
+    pactl unload-module module-rtp-send
+    pactl unload-module module-null-sink
+    pactl load-module module-null-sink sink_name=rtp channels=2 rate=44100
+    pactl load-module module-rtp-send source=$(node_name).monitor channels=2 rate=44100 format=s16be destination_ip=127.0.0.1 port=46000
+    pactl set-default-sink $(node_name)
 else
-    if [ $verbose = true ]; then
-        ffmpeg -v debug -re -i rtp://127.0.0.1:46000 -acodec mp3 -ab 128k -ac 2 -f rtp rtp://$d_ip:$port
-    else
-        ffmpeg -re -i rtp://127.0.0.1:46000 -acodec mp3 -ab 128k -ac 2 -f rtp rtp://$d_ip:$port
-    fi
+    src="$node_name.monitor"
+    fmt="pulse"
 fi
 
+if [ "$verbose" = true ]; then
+    verbose="-v debug"
+else
+    verbose=""
+fi
 
+ffmpeg $verbose -re -f $fmt -i $src -acodec mp3 -ab 128k -ac 2 -f rtp rtp://$d_ip:$port
 
+exit 0;
